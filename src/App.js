0f1efc82522a2de223184545125fb99b751af4b3
Usuario <usuario@example.com>
2025-11-09 18:58:55 -0300

    agregando css

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import LeitorFisico from "./componentes/LeitorFisico";
 
 function App() {
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
+    <div>
       <h1>ConsulteJá</h1>
       <p>Digite ou passe o código de barras para consultar o produto</p>
       <LeitorFisico />
