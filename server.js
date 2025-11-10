71fd9a6fba0282daf18beb0826debf4736b6b821
Usuario <usuario@example.com>
2025-11-09 21:37:30 -0300

    agregar produtos

--- server.js
+++ server.js
@@ -28,9 +28,45 @@ const produtos = {
   },
   "7896003700018": {
     nome: "Café Pilão Tradicional 500g",
-    descricao: "Café torrado e moído, sabor encorpado, pacote de 500g.",
+    descricao: "Café torrado e moído, sabor encorpado, pacote de 500g",
     preco: 15.49,
     imagem: "https://m.media-amazon.com/images/I/61-Sq6gYFEL._AC_SX569_.jpg",
+  },
+  "7891910000197": {
+    nome: "Feijão Carioca 1kg",
+    descricao: "Feijão carioca tipo 1, grãos selecionados, pacote de 1kg",
+    preco: 9.2,
+    imagem: "https://mambodelivery.vtexassets.com/arquivos/ids/167357/feijao-carioca-tipo-1-kicaldo-1kg.jpg?v=637883236210600000",
+  },
+  "7894900020010": {
+    nome: "Macarrão Espaguete 500g",
+    descricao: "Macarrão tipo espaguete tradicional 500g",
+    preco: 5.4,
+    imagem: "https://cristalalimentos.com.br/wp-content/uploads/2023/11/Safra-Espaguete-500g.jpg",
+  },
+  "7896003700025": {
+    nome: "Açúcar Refinado 1kg",
+    descricao: "Açúcar branco refinado, pacote de 1kg",
+    preco: 4.99,
+    imagem: "https://superprix.vteximg.com.br/arquivos/ids/192077/541354.jpg?v=637394065614300000",
+  },
+  "7891150060162": {
+    nome: "Óleo de Soja 900ml",
+    descricao: "Óleo vegetal de soja 900ml",
+    preco: 7.8,
+    imagem: "https://carrefourbrfood.vtexassets.com/arquivos/ids/211616/141836_1.jpg?v=637272514200130000",
+  },
+  "7894900700052": {
+    nome: "Arroz Integral 1kg",
+    descricao: "Arroz integral tipo 1, pacote de 1kg",
+    preco: 9.5,
+    imagem: "https://josapar.vteximg.com.br/arquivos/ids/157452-1000-1000/18483_1.jpg?v=638545905837500000",
+  },
+  "7894900010109": {
+    nome: "Leite Desnatado 1L",
+    descricao: "Leite desnatado pasteurizado 1 litro",
+    preco: 6.3,
+    imagem: "https://redemix.vteximg.com.br/arquivos/ids/213937-1000-1000/7898403782394.jpg?v=638350624515570000",
   },
 };
 
